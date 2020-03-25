c68c4368872b851de95663f9940d5762a3753258
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-03-25 12:28:33 +0200

    Peli tehtynä hyppyyn ja kiipeämiseen asti.

---
+++ Suvin 2020 kevää tasoloikka - oma harjoitus/Assets/Scripts/pelaaja.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelaaja : MonoBehaviour
+{
+    public float speed = 10f;                //Floating point variable to store the player's movement speed.
+    public float hyppyvoima = 5f;
+    public float kiipeysnopeus = 15f;
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private CircleCollider2D myFeet;
+
+    private float someScale;
+    private float direction = 1;
+    private float posX;
+
+    private float painovoima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<CircleCollider2D>();
+        someScale = transform.localScale.x; // assuming this is facing right
+        posX = transform.position.x;
+        painovoima = rb2d.gravityScale;
+    }
+
+    void Update()
+    {
+      
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Palauttavat 1 tai -1 riippuen siitä mihin suuntaan painetaan
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+
+        //Tekee painalluksista 2D vektorin
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        //Liikuttaa pelaajaa vektorin suuntaan annetulla nopeudella
+        rb2d.AddForce(movement * speed);
+
+        Debug.Log(" moveHorizontal - " + moveHorizontal);
+        Debug.Log(" transform.position.x - " + transform.position.x);
+
+        if (moveHorizontal != 0)
+        {
+            //Kääntäminen menosuuntaan 
+            if (transform.position.x < posX)
+            {
+                if (direction == 1)
+                {
+                    transform.localScale = new Vector2(-someScale, transform.localScale.y);
+                    direction = -1;
+                }
+            }
+            else
+            {
+                if (direction == -1)
+                {
+                    transform.localScale = new Vector2(someScale, transform.localScale.y);
+                    direction = 1;
+                }
+            }
+
+        }
+
+        posX = transform.position.x;
+        //Debug.Log(" posX - " + posX);
+        //Debug.Log(" direction - " + direction);
+
+        Debug.Log(myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")));
+        if (Input.GetButtonDown("Jump") && myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            Vector2 hyppy = new Vector2(0, hyppyvoima);
+            rb2d.AddForce(hyppy, ForceMode2D.Impulse);
+        }
+
+        Debug.Log(" osun portaisiin - " + myFeet.IsTouchingLayers(LayerMask.GetMask("Climping")));
+        if (myFeet.IsTouchingLayers(LayerMask.GetMask("Climping")))
+        {
+            float moveVertical = Input.GetAxis("Vertical");
+            rb2d.velocity = new Vector2(moveHorizontal, moveVertical * kiipeysnopeus);
+            rb2d.gravityScale = 0;
+        }
+        else
+        {
+            rb2d.gravityScale = painovoima;
+        }
+
+
+    }
+}
---
+++ Suvin 2020 kevää tasoloikka - oma harjoitus/Assets/Scripts/pelaaja_opetus.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelaaja_opetus : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float hyppyvoima = 5f;
+    public float kiipeilynopeus = 5f;
+
+    private Rigidbody2D rb2d;
+
+    private float direction;
+    private float posX;
+
+    private CircleCollider2D myFeet;
+    private float painovoima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        direction = 1;
+        posX = transform.position.x;
+        myFeet = GetComponent<CircleCollider2D>();
+        painovoima = rb2d.gravityScale;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+
+        if (moveHorizontal != 0)
+        {
+            if (transform.position.x < posX)
+            {
+                /*Debug.Log("Moving left - " + transform.position.x);*/
+                if (direction == 1)
+                {
+                    transform.localScale = new Vector2(-1, transform.localScale.y);
+                    direction = -1;
+                }
+            }
+            else
+            {
+                /*Debug.Log("Moving right - " + transform.position.x);*/
+                if (direction == -1)
+                {
+                    transform.localScale = new Vector2(1, transform.localScale.y);
+                    direction = 1;
+                }
+            }
+
+            posX = transform.position.x;
+        }
+
+        Debug.Log("jalat osuvat maahan" + myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")));
+        if (Input.GetButtonDown("Jump") && myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            Vector2 hyppy = new Vector2(0, hyppyvoima);
+            rb2d.AddForce(hyppy, ForceMode2D.Impulse);
+        }
+
+        Debug.Log("jalat osuvat portaisiin" + myFeet.IsTouchingLayers(LayerMask.GetMask("Climping")));
+
+        if (myFeet.IsTouchingLayers(LayerMask.GetMask("Climping")))
+        {
+            float moveVertical = Input.GetAxis("Vertical");
+            rb2d.velocity = new Vector2(moveHorizontal, moveVertical * kiipeilynopeus);
+            rb2d.gravityScale = 0;
+        }
+        else
+        {
+            rb2d.gravityScale = painovoima;
+        }
+    }
+}
